5e67cbc02f6799825327939b925074faf4d2c9b9
Xiaolei Jiang <xjiang866@gmail.com>
2022-03-29 19:48:06 -0700

    created Engine class

---
+++ Src/Core/engine.cpp
@@ -0,0 +1,27 @@
+#include "engine.h"
+
+bool Engine::Init()
+{
+	return false;
+}
+
+bool Engine::Clean()
+{
+	return false;
+}
+
+void Engine::Quit()
+{
+}
+
+void Engine::Update(float dt)
+{
+}
+
+void Engine::Render()
+{
+}
+
+void Engine::Events()
+{
+}
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,7 +1,12 @@
-#include <SDL.h>
-#include <SDL_image.h>
+#include "Core/engine.h"
+
 
 int main(int argc, char* args[])
 {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		cout << "SDL_Init failed" << endl;
+	}
+
 	return 0;
-}
+} 
